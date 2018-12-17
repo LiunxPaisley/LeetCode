3a73e5c11d3722215f77f78c0d3bc14c007e78e2
LiunxPaisley <liunxalkaid@outlook.com>
2018-12-17 19:49:40 +0800

    2018年12月17日星期一19点49分

--- 25. Reverse Nodes in k-Group/25. Reverse Nodes in k-Group/25. Reverse Nodes in k-Group.cpp
+++ 25. Reverse Nodes in k-Group/25. Reverse Nodes in k-Group/25. Reverse Nodes in k-Group.cpp
@@ -13,13 +13,64 @@ struct ListNode {
 class Solution {
 public:
 	ListNode* reverseKGroup(ListNode* head, int k) {
+		if (k == 1 || !head) {
+			return head;
+		}
+		ListNode* end = head;
+		ListNode* prev = NULL;
+		ListNode* begin = NULL;
+		
+		bool exit = false;
+		bool first = true;
+		while (!exit) {
+			begin = end;
+			for (int j = 0; j < k - 1; ++j) {
+				if (!end || !(end->next)) {
+					exit = true;
+					break;
+				}
+				end = end->next;
+				
+			}
+			
+			if (exit) continue;
+			for (int i = 0; i < k - 1; ++i) {
+				ListNode* temp = begin;
+				begin = begin->next;
+				if (prev) {
+					prev->next = begin;
+				}
+				temp->next = end->next;
+				end->next = temp;
+				//begin = begin->next;
+				//end = end->next;
+			}
+			if (first) {
+				head = end;
+				first = false;
+			}
+			
+			//ListNode* iter = head;
+			//while (iter) {
+			//	cout << iter->val << " ";
+			//	iter = iter->next;
+			//}
+			//cout << endl;
 
+			for (int h = 0; h < k-1; ++h) {
+				end = end->next;
+			}
+			prev = end;
+			end = end->next;
+		}
+
+		return head;
 	}
 };
 
 int main() {
 	Solution sln;
-	vector<int> v{ 1,2,3,4,5,6,7 };
+	vector<int> v{ 1,2,3,4,5 };
 
 	ListNode* head;
 	ListNode** p = &head;
@@ -28,12 +79,13 @@ int main() {
 		p = &((*p)->next);
 	}
 
-	int k = 3;
+	int k = 2;
 	ListNode* result = sln.reverseKGroup(head, k);
 
-	ListNode* ptr = result;
-	while (ptr) {
-		cout << ptr->val << " ";
-		ptr = ptr->next;
+	ListNode* end = result;
+	while (end) {
+		cout << end->val << " ";
+		end = end->next;
 	}
+	cout << endl;
 }
