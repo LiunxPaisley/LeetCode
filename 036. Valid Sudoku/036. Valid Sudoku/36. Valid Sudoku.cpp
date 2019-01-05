240c690fba0690930b867c8c00a7fdc2fd290060
LiunxPaisley <liunxalkaid@outlook.com>
2019-01-05 14:00:49 +0800

    第36题

---
+++ 036. Valid Sudoku/036. Valid Sudoku/36. Valid Sudoku.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <array>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <list>
+#include <deque>
+#include <tuple>
+
+#include <algorithm>
+#include <utility>
+#include <memory>
+#include <limits>
+#include <functional>
+
+#include <cstring> 
+#include <cstdio>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isValidSudoku(vector<vector<char>>& board) {
+		map<int, int> row;
+		map<int, int> col;
+		map<int, int> sq;
+		for (int i = 0; i < 9; ++i) {
+			for (int j = 0; j < 9; ++j) {
+
+			}
+		}
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+
+			}
+		}
+	}
+};
+
+
+int main() {
+	Solution sln;
+}
