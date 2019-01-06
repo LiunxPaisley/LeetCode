e45fa761bf9646385e3d7f7ec76205ed33520fc5
LiunxPaisley <liunxalkaid@outlook.com>
2019-01-06 17:08:32 +0800

    第39题
    用了回溯的方法，大量减少了执行时间

--- 039. Combination Sum/039. Combination Sum/39. Combination Sum.cpp
+++ 039. Combination Sum/039. Combination Sum/39. Combination Sum.cpp
@@ -30,28 +30,31 @@ using namespace std;
 
 class Solution {
 public:
-	void findSum(vector<int>& candidates, int target, vector<int>& temp, unordered_set<vector<int>>& result) {
+	void findSum(vector<int>& candidates, int target, vector<int>& temp, set<vector<int>>& result, int idx) {
 		int can_size = candidates.size();
-		for (int i = 0; i < can_size; ++i) {
-			vector<int> vec = temp;
+		for (int i = idx; i < can_size; ++i) {
+			vector<int>& vec = temp;
+			int size = vec.size();
 			int next = target - candidates[i];
 			if (next > 0) {
 				vec.push_back(candidates[i]);
-				findSum(candidates, next, vec, result);
+				findSum(candidates, next, vec, result, i);
 			} else if(next == 0){
-				temp.push_back(candidates[i]);
-				result.insert(temp);
-				return;
-			} else {
+				vec.push_back(candidates[i]);
+				result.insert(vec);
 				return;
 			}
-			
+			vec.resize(size);
 		}
 	}
 	vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-		unordered_set<vector<int>> result;
+		if (candidates.size() == 0) {
+			return {};
+		}
+		sort(candidates.begin(), candidates.end());
+		set<vector<int>> result;
 		vector<int> temp;
-		findSum(candidates, target, temp, result);
+		findSum(candidates, target, temp, result, 0);
 		return vector<vector<int>>(result.begin(), result.end());
 	}
 };
@@ -72,7 +75,7 @@ int main() {
 		}
 		cout << endl;
 	}
-
+	cout << endl;
 
 	candidates = { 2,3,5 };
 	target = 8;
@@ -83,4 +86,27 @@ int main() {
 		}
 		cout << endl;
 	}
+	cout << endl;
+
+	candidates = { 8,7,4,3 };
+	target = 11;
+	result = sln.combinationSum(candidates, target);
+	for (auto&& x : result) {
+		for (auto&& y : x) {
+			cout << y << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	candidates = { 4, 2, 8 };
+	target = 8;
+	result = sln.combinationSum(candidates, target);
+	for (auto&& x : result) {
+		for (auto&& y : x) {
+			cout << y << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
