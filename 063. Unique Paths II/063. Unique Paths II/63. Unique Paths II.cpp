273392ed81141740244b5d90c56c143983d20dc2
LiunxPailsley <LiunxAlkaid@outlook.com>
2019-01-08 22:39:36 +0800

    第63题，动态规划

--- 063. Unique Paths II/063. Unique Paths II/63. Unique Paths II.cpp
+++ 063. Unique Paths II/063. Unique Paths II/63. Unique Paths II.cpp
@@ -34,14 +34,41 @@ public:
 	int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
 		int m = obstacleGrid.size();
 		int n = obstacleGrid[0].size();
-		int step = m + n - 1;
+		//int step = m + n - 1;
 
-		int result;
-		vector<vector<int>> record(m, vector<int>(0, n));
-		record[0][0] = 1;
-		for (int i = 0; i < step; ++i) {
-				
+		if (obstacleGrid[0][0] == 1) {
+			return 0;
 		}
+		if (obstacleGrid[m - 1][n - 1] == 1) {
+			return 0;
+		}
+		//queue<pair<int, int>> stk;
+
+		vector<vector<int>> record(m, vector<int>(n, 0));
+		record[0][0] = 1;
+
+		for (int i = 0; i < m; ++i) {
+			for (int j = 0; j < n; ++j) {
+				if (obstacleGrid[i][j] != 1) {
+					if (j - 1 >= 0 && obstacleGrid[i][j - 1] != 1) {
+						record[i][j] += record[i][j - 1];
+					}
+					if (i - 1 >= 0 && obstacleGrid[i - 1][j] != 1) {
+						record[i][j] += record[i - 1][j];
+					}
+				}
+			}
+		}
+
+		for (auto&& x : record) {
+			for (auto&& y : x) {
+				cout << y << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
+
+		return record[m - 1][n - 1];
 	}
 };
 
@@ -56,5 +83,21 @@ int main() {
 					 {0,0,0} };
 	result = sln.uniquePathsWithObstacles(obstacleGrid);
 	cout << result << endl;
+
+	obstacleGrid = { {0, 0, 0, 0}, 
+					{ 0, 1, 0, 0 }, 
+					{ 0, 0, 0, 0 }, 
+					{ 0, 0, 1, 0 }, 
+					{ 0, 0, 0, 0 }};
+	result = sln.uniquePathsWithObstacles(obstacleGrid);
+	cout << result << endl;
 	
+
+	obstacleGrid = { {0, 1} };
+	result = sln.uniquePathsWithObstacles(obstacleGrid);
+	cout << result << endl;
+
+	obstacleGrid = { {1} };
+	result = sln.uniquePathsWithObstacles(obstacleGrid);
+	cout << result << endl;
 }
