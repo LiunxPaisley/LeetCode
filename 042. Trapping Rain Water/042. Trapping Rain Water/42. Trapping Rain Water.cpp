af1736d5c579fb71719171257e5f12efe6c52477
LiunxPaisley <liunxalkaid@outlook.com>
2019-01-21 20:40:42 +0800

    第41题，Runtime: 4 ms, faster than 100.00% of C++ online submissions for Trapping Rain Water.

--- 042. Trapping Rain Water/042. Trapping Rain Water/42. Trapping Rain Water.cpp
+++ 042. Trapping Rain Water/042. Trapping Rain Water/42. Trapping Rain Water.cpp
@@ -31,11 +31,49 @@ using namespace std;
 class Solution {
 public:
 	int trap(vector<int>& height) {
+		int result = 0;
 
+		int height_size = height.size();
+		for (int i = 0; i < height_size;) {
+			// find bigger one
+			int flag = false;
+			int j;
+			for (j = i+1; j < height_size; ++j) {
+				if (height[j] >= height[i]) {
+					result += (j - i - 1)*height[i];
+					//i = j;
+					flag = true;
+					break;
+				}
+			}
+			cout << "result = " << result << endl;
+			cout << "i = " << i << endl;
+			// find max one
+			if (!flag) {
+				vector<int>::iterator iter = max_element(height.begin()+(i+1), height.end());
+				if (iter == height.end()) {
+					break;
+				}
+				j = iter - height.begin();
+				cout << "j = " << j << endl;
+				result += (j - i - 1)*height[j];
+			}
+			++i;
+			while (i != j && i < height_size) {
+				result -= height[i];
+				++i;
+			}
+		}
+		return result;
 	}
 };
 
 
 int main() {
 	Solution sln;
+	vector<int> height;
+	//int result;
+
+	height = { 0,1,0,2,1,0,1,3,2,1,2,1 };
+	cout << sln.trap(height) << endl;
 }
