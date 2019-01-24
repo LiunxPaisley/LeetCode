37deb442b2732617c9975daf21213ce2e9dd24b8
LiunxPaisley <liunxalkaid@outlook.com>
2019-01-24 11:36:28 +0800

    第44题，动态规划，Runtime: 40 ms, faster than 63.99% of C++ online submissions for Wildcard Matching.

--- 044. Wildcard Matching/044. Wildcard Matching/44. Wildcard Matching.cpp
+++ 044. Wildcard Matching/044. Wildcard Matching/44. Wildcard Matching.cpp
@@ -33,35 +33,60 @@ public:
 	bool isMatch(string s, string p) {
 		int len_s = s.size();
 		int len_p = p.size();
-		int i = 0, j = 0;
-		while (i < len_s && j < len_p) {
-			if (p[j] == '?') {
-				++i;
-				++j;
-			} else if (p[j] == '*') {
-				int k = i;
-				while (s[k] != p[j + 1]) {
-					++k;
-				}
-				
-			} else {
-				if (s[i] != p[j]) {
-					return false;
+		vector<vector<bool>> rec(len_s + 1, vector<bool>(len_p + 1, false));
+		rec[0][0] = true;
+		for (int i = 1; i < len_p + 1; ++i) {
+			if (p[i - 1] == '*') {
+				rec[0][i] = rec[0][i - 1];
+			}
+		}
+		for (int i = 1; i < len_s + 1; ++i) {
+			for (int j = 1; j < len_p + 1; ++j) {
+				if (p[j - 1] == '?') {
+					rec[i][j] = rec[i - 1][j - 1];
+				} else if (p[j - 1] == '*') {
+					rec[i][j] = rec[i][j - 1] || rec[i - 1][j];
 				} else {
-					++i;
-					++j;
+					rec[i][j] = (s[i - 1] == p[j - 1]) && rec[i - 1][j - 1];
 				}
 			}
 		}
-		if (i == len_s && j == len_p) {
-			return true;
-		} else {
-			return false;
-		}
+
+		//for (auto&& x : rec) {
+		//	for (auto&& y : x) {
+		//		cout << y << " ";
+		//	}
+		//	cout << endl;
+		//}
+
+		return rec[len_s][len_p];
 	}
 };
 
 
 int main() {
 	Solution sln;
+	string s;
+	string p;
+
+	s = "aa", p = "a";
+	cout << sln.isMatch(s, p) << "\n\n";
+
+	s = "aa", p = "*";
+	cout << sln.isMatch(s, p) << "\n\n";
+
+	s = "cb", p = "?a";
+	cout << sln.isMatch(s, p) << "\n\n";
+
+	s = "adceb", p = "*a*b";
+	cout << sln.isMatch(s, p) << "\n\n";
+
+	s = "acdcb", p = "a*c?b";
+	cout << sln.isMatch(s, p) << "\n\n";
+
+	s = "aaaaaa", p = "a**a?";
+	cout << sln.isMatch(s, p) << "\n\n";
+
+	s = "aaaaaa", p = "a**b?";
+	cout << sln.isMatch(s, p) << "\n\n";
 }
