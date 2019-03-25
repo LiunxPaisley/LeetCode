8cb413d8f79c39516957d9c1112cdc6d474a9229
LiunxPailsley <LiunxAlkaid@outlook.com>
2019-03-25 22:54:13 +0800

    第100题，Runtime: 4 ms, faster than 100.00% of C++ online submissions for Same Tree. Memory Usage: 10 MB, less than 83.10% of C++ online submissions for Same Tree.

---
+++ 100. Same Tree/100. Same Tree/100. Same Tree.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <array>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <list>
+#include <deque>
+#include <tuple>
+
+#include <algorithm>
+#include <utility>
+#include <memory>
+#include <limits>
+#include <functional>
+#include <numeric>
+
+#include <cstring> 
+#include <cstdio>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+
+/**
+* Definition for a binary tree node.
+*/
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void make_tree(TreeNode** root, vector<int>& nums, int& i) {
+	if (nums[i] != INT_MIN) {
+		*root = new TreeNode(nums[i]);
+		make_tree(&((*root)->left), nums, ++i);
+		make_tree(&((*root)->right), nums, ++i);
+	}
+}
+
+void make_tree(TreeNode** root, vector<int>& nums) {
+	queue<TreeNode**> que;
+	que.push(root);
+	int i = 0;
+	while (i < nums.size()) {
+		TreeNode** root = que.front();
+		if (nums[i] != INT_MIN) {
+			*root = new TreeNode(nums[i]);
+			que.push(&((*root)->left));
+			que.push(&((*root)->right));
+		}
+		i++;
+		que.pop();
+	}
+}
+
+void print_tree(TreeNode* root) {
+	if (root) {
+		print_tree(root->left);
+		cout << root->val << " ";
+		print_tree(root->right);
+	} else {
+		cout << "# ";
+	}
+}
+
+class Solution {
+public:
+	bool isSameTree(TreeNode* p, TreeNode* q) {
+		if (!p && q) return false;
+		if (p && !q) return false;
+		if (p && q) {
+			if (p->val != q->val) {
+				return false;
+			}
+			if (!isSameTree(p->left, q->left)) {
+				return false;
+			}
+			if (!isSameTree(p->right, q->right)) {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+
+int main() {
+	Solution sln;
+}
