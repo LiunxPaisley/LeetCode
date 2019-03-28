4560b2459b1fef0ade62e15e275b19770a875a66
LiunxPaisley <liunxalkaid@outlook.com>
2019-03-28 11:34:04 +0800

    第107题，Runtime: 8 ms, faster than 100.00% of C++ online submissions for Binary Tree Level Order Traversal II. Memory Usage: 14 MB, less than 48.40% of C++ online submissions for Binary Tree Level Order Traversal II.

---
+++ 107. Binary Tree Level Order Traversal II/107. Binary Tree Level Order Traversal II/107. Binary Tree Level Order Traversal II.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <array>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <list>
+#include <deque>
+#include <tuple>
+
+#include <algorithm>
+#include <utility>
+#include <memory>
+#include <limits>
+#include <functional>
+#include <numeric>
+
+#include <cstring> 
+#include <cstdio>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+
+/**
+* Definition for a binary tree node.
+*/
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void make_tree(TreeNode** root, vector<int>& nums, int& i) {
+	if (nums[i] != INT_MIN) {
+		*root = new TreeNode(nums[i]);
+		make_tree(&((*root)->left), nums, ++i);
+		make_tree(&((*root)->right), nums, ++i);
+	}
+}
+
+void make_tree(TreeNode** root, vector<int>& nums) {
+	queue<TreeNode**> que;
+	que.push(root);
+	int i = 0;
+	while (i < nums.size()) {
+		TreeNode** root = que.front();
+		if (nums[i] != INT_MIN) {
+			*root = new TreeNode(nums[i]);
+			que.push(&((*root)->left));
+			que.push(&((*root)->right));
+		}
+		i++;
+		que.pop();
+	}
+}
+
+void print_tree(TreeNode* root) {
+	if (root) {
+		print_tree(root->left);
+		cout << root->val << " ";
+		print_tree(root->right);
+	} else {
+		cout << "# ";
+	}
+}
+
+class Solution {
+public:
+	vector<vector<int>> levelOrderBottom(TreeNode* root) {
+		if (!root) return {};
+		queue<TreeNode*> que;
+		vector<vector<int>> res;
+		que.push(root);
+		int count = 1;
+		while (!que.empty()) {
+			vector<int> vec{};
+			int cnt = count;
+			count = 0;
+			for (int i = 0; i < cnt; ++i) {
+				TreeNode* tmp = que.front();
+				vec.push_back(tmp->val);
+				que.pop();
+				if (tmp->left) {
+					que.push(tmp->left);
+					count++;
+				}
+				if (tmp->right) {
+					que.push(tmp->right);
+					count++;
+				}
+			}
+			res.push_back(vec);
+		}
+		reverse(res.begin(), res.end());
+		return res;
+	}
+};
+
+
+int main() {
+	Solution sln;
+}
