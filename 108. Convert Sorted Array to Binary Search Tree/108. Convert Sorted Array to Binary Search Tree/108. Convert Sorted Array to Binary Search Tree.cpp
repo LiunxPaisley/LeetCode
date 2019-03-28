533dc69479af4d3f80554c8e807de42739cb5664
LiunxPaisley <liunxalkaid@outlook.com>
2019-03-28 11:47:12 +0800

    第108，Runtime: 24 ms, faster than 85.84% of C++ online submissions for Convert Sorted Array to Binary Search Tree. Memory Usage: 21 MB, less than 92.58% of C++ online submissions for Convert Sorted Array to Binary Search Tree.

---
+++ 108. Convert Sorted Array to Binary Search Tree/108. Convert Sorted Array to Binary Search Tree/108. Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <array>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <list>
+#include <deque>
+#include <tuple>
+
+#include <algorithm>
+#include <utility>
+#include <memory>
+#include <limits>
+#include <functional>
+#include <numeric>
+
+#include <cstring> 
+#include <cstdio>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+
+/**
+* Definition for a binary tree node.
+*/
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void make_tree(TreeNode** root, vector<int>& nums, int& i) {
+	if (nums[i] != INT_MIN) {
+		*root = new TreeNode(nums[i]);
+		make_tree(&((*root)->left), nums, ++i);
+		make_tree(&((*root)->right), nums, ++i);
+	}
+}
+
+void make_tree(TreeNode** root, vector<int>& nums) {
+	queue<TreeNode**> que;
+	que.push(root);
+	int i = 0;
+	while (i < nums.size()) {
+		TreeNode** root = que.front();
+		if (nums[i] != INT_MIN) {
+			*root = new TreeNode(nums[i]);
+			que.push(&((*root)->left));
+			que.push(&((*root)->right));
+		}
+		i++;
+		que.pop();
+	}
+}
+
+void print_tree(TreeNode* root) {
+	if (root) {
+		print_tree(root->left);
+		cout << root->val << " ";
+		print_tree(root->right);
+	} else {
+		cout << "# ";
+	}
+}
+
+class Solution {
+public:
+	TreeNode* sortedArrayToBST(vector<int>& nums) {
+		return sortedArrayToBST(nums, 0, nums.size() - 1);
+	}
+private:
+	TreeNode* sortedArrayToBST(vector<int>& nums, int l, int r) {
+		if (r < l) return NULL;
+		int mid = l + (r - l) / 2;
+		TreeNode* root = new TreeNode(nums[mid]);
+		root->left = sortedArrayToBST(nums, l, mid - 1);
+		root->right = sortedArrayToBST(nums, mid + 1, r);
+		return root;
+	}
+};
+
+
+int main() {
+	Solution sln;
+	vector<int> nums{};
+	TreeNode* root;
+
+	nums = { -10,-3,0,5,9 };
+	root = sln.sortedArrayToBST(nums);
+	print_tree(root);
+	cout << endl;
+}
