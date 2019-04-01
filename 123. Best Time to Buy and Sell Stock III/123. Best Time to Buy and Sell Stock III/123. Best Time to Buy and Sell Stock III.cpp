e7db10ff00af7e40143df4a177fb2a6317c6ec1e
LiunxPailsley <LiunxAlkaid@outlook.com>
2019-04-01 22:06:08 +0800

    第123题，Runtime: 20 ms, faster than 18.33% of C++ online submissions for Best Time to Buy and Sell Stock III. Memory Usage: 13.5 MB, less than 5.03% of C++ online submissions for Best Time to Buy and Sell Stock III.

--- 123. Best Time to Buy and Sell Stock III/123. Best Time to Buy and Sell Stock III/123. Best Time to Buy and Sell Stock III.cpp
+++ 123. Best Time to Buy and Sell Stock III/123. Best Time to Buy and Sell Stock III/123. Best Time to Buy and Sell Stock III.cpp
@@ -31,43 +31,18 @@ using namespace std;
 class Solution {
 public:
 	int maxProfit(vector<int>& prices) {
-		if (prices.size() <= 1) {
-			return 0;
-		}
-		vector<int> max_(prices.size()-1, 0);
-		for (int i = 0; i < max_.size(); ++i) {
-			max_[i] = max_profile(prices, 0, i + 2) + max_profile(prices, i + 2, prices.size());
-		}
-
-		for (auto&& x : max_) {
-			cout << x << " ";
-		}
-		cout << endl;
-
-		return *max_element(max_.begin(), max_.end());
-	}
-private:
-	int max_profile(vector<int>& prices, int start, int end) {
-		if (start >= end) {
-			return 0;
-		}
-		vector<int> record(end - start, 0);
-		for (int i = 0; i < record.size()-1; ++i) {
-			for (int j = i+1; j < record.size(); ++j) {
-				int profile = prices[start + j] - prices[start + i];
-				if (profile > record[i]) {
-					record[i] = profile;
-				}
+		if (prices.empty()) return 0;
+		int n = prices.size();
+		vector<vector<int>> g(n, vector<int>(3, 0));
+		vector<vector<int>> l(n, vector<int>(3, 0));
+		for (int i = 1; i < prices.size(); ++i) {
+			int diff = prices[i] - prices[i - 1];
+			for (int j = 1; j <= 2; ++j) {
+				l[i][j] = max(g[i - 1][j - 1] + max(diff, 0), l[i - 1][j] + diff);
+				g[i][j] = max(l[i][j], g[i - 1][j]);
 			}
 		}
-
-		cout << "start: " << start << ", end: " << end << ", ";
-		for (auto&& x : record) {
-			cout << x << " ";
-		}
-		cout << endl;
-		
-		return *max_element(record.begin(), record.end());
+		return g[n - 1][2];
 	}
 };
 
