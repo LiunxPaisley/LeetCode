1d63bcd31e22ee91e0186d74114054ae9284bbc4
LiunxPaisley <liunxalkaid@outlook.com>
2019-04-03 11:01:16 +0800

    第215题，Runtime: 84 ms, faster than 10.70% of C++ online submissions for Kth Largest Element in an Array. Memory Usage: 9.2 MB, less than 93.88% of C++ online submissions for Kth Largest Element in an Array.

--- 215. Kth Largest Element in an Array/215. Kth Largest Element in an Array/215. Kth Largest Element in an Array.cpp
+++ 215. Kth Largest Element in an Array/215. Kth Largest Element in an Array/215. Kth Largest Element in an Array.cpp
@@ -32,6 +32,36 @@ using namespace std;
 class Solution {
 public:
 	int findKthLargest(vector<int>& nums, int k) {
+		int left = 0;
+		int right = nums.size() - 1;
+		for (;;) {
+			int pos = partition(nums, left, right);
+			if (pos == k - 1) {
+				return nums[pos];
+			} else if (pos > k - 1) {
+				right = pos - 1;
+			} else {
+				left = pos + 1;
+			}
+		}
+	}
+	int partition(vector<int>& nums, int i, int j) {
+		int pivot = nums[i];
+		int left = i + 1;
+		int right = j;
+		while (left <= right) {
+			if (nums[left] < pivot && nums[right] > pivot) {
+				swap(nums[left], nums[right]);
+				left++;
+				right--;
+			}
+			if (nums[left] >= pivot) left++;
+			if (nums[right] <= pivot) right--;
+		}
+		swap(nums[i], nums[right]);
+		return right;
+	}
+	int findKthLargest1(vector<int>& nums, int k) {
 		priority_queue<int> que(nums.begin(), nums.end());
 		for (int i = 0; i < k - 1; ++i) {
 			que.pop();
