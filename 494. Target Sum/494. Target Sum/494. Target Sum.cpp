3fb34a4d09aee401d2977b2fdafa247d6f1ebe7f
LiunxPaisley <liunxalkaid@outlook.com>
2019-04-20 15:19:55 +0800

    第494题，Runtime: 4 ms, faster than 100.00% of C++ online submissions for Target Sum. Memory Usage: 8.8 MB, less than 100.00% of C++ online submissions for Target Sum.

--- 494. Target Sum/494. Target Sum/494. Target Sum.cpp
+++ 494. Target Sum/494. Target Sum/494. Target Sum.cpp
@@ -32,7 +32,30 @@ using namespace std;
 
 class Solution {
 public:
+	// sum(Positive)-sum(Negative) = target 
+	// sum(Positive)+sum(Negative)+sum(Positive)-sum(Negative) = target + sum(Positive)+sum(Negative)
+	// 2*sum(Positive) = target+sum(nums)
+	// sum(P) = (target + sum(nums))/2
 	int findTargetSumWays(vector<int>& nums, int S) {
+		int sum = accumulate(nums.begin(), nums.end(), 0);
+		if (sum < S) return 0;
+		if ((S + sum) % 2) return 0;
+		return subsetsum(nums, (S + sum) / 2);
+	}
+private:
+	// 子数组问题
+	int subsetsum(vector<int> & nums, int target) {
+		vector<int> dp(target + 1, 0);
+		dp[0] = 1;
+		for (int i = 0; i < nums.size(); ++i) {
+			for (int j = target; j >= nums[i]; --j) {
+				dp[j] += dp[j - nums[i]];
+			}
+		}
+		return dp[target];
+	}
+public:
+	int findTargetSumWays1(vector<int>& nums, int S) {
 		int res = 0;
 		select(nums, 0, 0, res, S);
 		return res;
