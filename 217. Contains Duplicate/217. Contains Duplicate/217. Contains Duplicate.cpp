f494613abae2e6f432265c3df36fcbc229456549
LiunxPaisley <liunxalkaid@outlook.com>
2019-06-06 10:03:00 +0800

    第217题，Runtime: 32 ms, faster than 94.61% of C++ online submissions for Contains Duplicate. Memory Usage: 16.6 MB, less than 22.38% of C++ online submissions for Contains Duplicate.

---
+++ 217. Contains Duplicate/217. Contains Duplicate/217. Contains Duplicate.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <array>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <list>
+#include <deque>
+#include <tuple>
+
+#include <algorithm>
+#include <utility>
+#include <memory>
+#include <limits>
+#include <functional>
+#include <numeric>
+
+#include <cstring> 
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cctype>
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * Runtime: 32 ms, faster than 94.61% of C++ online submissions for Contains Duplicate.
+     * Memory Usage: 16.6 MB, less than 22.38% of C++ online submissions for Contains Duplicate.
+     */
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (m[nums[i]]) return true;
+            m[nums[i]]++;
+        }
+        return false;
+    }
+};
+
+
+int main() {
+    Solution sln;
+}
